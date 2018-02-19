6d0cbc165595a97bb0822888b522f2a7452875f0
Rafael Berger <GeorgeJohnson24790@gmail.com>
2018-02-19 13:00:53 -0500

    added Configuration for injecting the db context

--- Data/EduSmartContext.cs
+++ Data/EduSmartContext.cs
@@ -9,14 +9,17 @@ namespace EduSmart.Data
         public virtual DbSet<AspNetUsers> AspNetUsers { get; set; }
         public DbSet<Payment> Payments { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EduSmart;Trusted_Connection=True;");
-            }
-        }
+        //        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //        {
+        //            if (!optionsBuilder.IsConfigured)
+        //            {
+        //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+        //                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EduSmart;Trusted_Connection=True;");
+        //            }
+        //        }
+        public EduSmartContext(DbContextOptions<EduSmartContext> options)
+            : base(options)
+        { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
