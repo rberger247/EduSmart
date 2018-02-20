6c2db824f4bce916e3952dc76acb8d8bf9ba4dde
Rafael Berger <GeorgeJohnson24790@gmail.com>
2018-02-20 13:40:31 -0500

    Reinstalled Dependencies

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -7,7 +7,7 @@ import { AuthService } from './services/auth.service';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+//import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { PaymentComponent } from './components/payment/payment.component';
 import { Router } from '@angular/router'; 
@@ -17,7 +17,7 @@ import { Router } from '@angular/router';
         AppComponent,
         NavMenuComponent,
         CounterComponent,
-        FetchDataComponent,
+        //FetchDataComponent,
         HomeComponent,
         PaymentComponent
     ],
@@ -29,7 +29,7 @@ import { Router } from '@angular/router';
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
+            //{ path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
 
